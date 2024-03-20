0012d75612e90e7adf33c147264336c98626b289
nishuyada <nishuyadav12122006@gmail.com>
2024-03-20 17:05:03 +0530

    even odd

---
+++ evenOdd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int b;
+    cout<<"enter the number";
+    cin>>b;
+    if(b%2==0)
+    {
+        cout<<"even number";
+
+    }
+    else{
+        cout<<"odd number";
+    }
+
+    return 0;
+}
